8779f027ae333953baac663eb7191d66cb76336f
GABRIEL CESAR PEREIRA <gabriel.pereira@rn.local>
2024-12-20 17:37:21 -0300

    terminando configuração de página de admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,16 @@
+
 from django.contrib import admin
+from galeria.models import Fotografia
 
 # Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'categoria', 'publicada')
+    list_display_links = ('nome',)
+    search_fields = ('nome',)
+    list_filter = ('categoria',)
+    list_per_page = 10
+    list_editable = ('publicada',)
+    
+admin.site.register(Fotografia, ListandoFotografias)
+
+
--- galeria/views.py
+++ galeria/views.py
@@ -3,9 +3,19 @@ from django.http import HttpResponse
 from galeria.models import Fotografia
 
 def index(request):
-    fotografias = Fotografia.objects.all()
+    fotografias = Fotografia.objects.filter(publicada=True).order_by('-data_fotografia')
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.filter(publicada=True).order_by('-data_fotografia')
+    
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar: 
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    return render (request, "galeria/buscar.html", {"cards": fotografias})
